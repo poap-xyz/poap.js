fdc6301629855569f6183dd0bde91f63f39ff693
Nacho <nachonegrocaino@gmail.com>
2023-05-22 10:45:54 -0300

    Add env vars to moments example

--- examples/moments/backend/src/index.ts
+++ examples/moments/backend/src/index.ts
@@ -9,19 +9,25 @@ import { create_moment } from './methods/create_moment';
 import { fetch_multiple_moments } from './methods/fetch_multiple_moments';
 import { fetch_single_moment } from './methods/fetch_single_moment';
 import { fetch_moments_by_drop_ids } from './methods/fetch_moments_by_drop_ids';
+import { getRequiredEnvVar } from './methods/get_required_env_var';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function main(): Promise<void> {
+  const momentsApi = new PoapMomentsApi({
+    // You need to provide an authentication provider for write operations
+    authenticationProvider: new AuthenticationProviderHttp(
+      getRequiredEnvVar('CLIENT_ID'),
+      getRequiredEnvVar('CLIENT_SECRET'),
+    ),
+  });
+
+  // We use Compass for read operations
+  const compass = new PoapCompass('your_api_key');
+
   // Use your library here
-  const client = new MomentsClient(
-    new PoapMomentsApi({
-      // You need to provide an authentication provider for write operations
-      authenticationProvider: new AuthenticationProviderHttp(
-        '<CLIENT_ID>',
-        '<CLIENT_SECRET>',
-      ),
-    }),
-    new PoapCompass('your_api_key'),
-  );
+  const client = new MomentsClient(momentsApi, compass);
   // Create Moment
   await create_moment(client);
   // Fetch multiple moments
