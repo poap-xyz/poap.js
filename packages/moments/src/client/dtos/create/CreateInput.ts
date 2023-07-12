9a4ab635d9a5ef89e1464e07a85ce3c4a245e8ad
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2023-07-12 13:10:44 -0300

    Feature/moments file upload progress (#41)
    * Add on file progress update callbacks
    
    * bump package versions
    
    * Fix comments

--- packages/moments/src/client/dtos/create/CreateInput.ts
+++ packages/moments/src/client/dtos/create/CreateInput.ts
@@ -9,6 +9,8 @@ import { CreateSteps } from './CreateSteps';
  * @property {string} author - The author of the moment.
  * @property {string} timeOut - The amount of time to wait until media is processed.
  * @property {string} fileType - The type of the file.
+ * @property {(step: CreateSteps) => void | Promise<void>} [onStepUpdate] - Optional callback function to be called when the step changes.
+ * @property {(progress: number) => void | Promise<void>} [onFileProgress] - Optional callback function to be called when the file upload progress change - progress is a number between 0 and 1.
  */
 export interface CreateMomentInput {
   dropId: number;
@@ -18,4 +20,5 @@ export interface CreateMomentInput {
   timeOut?: number;
   fileType: string;
   onStepUpdate?: (step: CreateSteps) => void | Promise<void>;
+  onFileUploadProgress?: (progress: number) => void | Promise<void>;
 }
--- packages/providers/src/index.ts
+++ packages/providers/src/index.ts
@@ -1,4 +1,5 @@
 export { InvalidMediaFileError } from './core/PoapMomentsApi/errors/InvalidMediaFileError';
 export { DropResponse } from './ports/DropApiProvider/Types/response';
+export { AuthenticationProvider } from './ports/AuthenticationProvider/AuthenticationProvider';
 export * from './ports';
 export * from './core';
