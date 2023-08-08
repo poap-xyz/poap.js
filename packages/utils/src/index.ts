1f5194ef9295ac28e5b1132e069af42fd0ed0b2e
rlajous <rodrigo@lajous.com.ar>
2023-08-08 12:26:23 +0200

    Created new interface to extend for inputs

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,6 @@
 export { Chain } from './types/chain';
 export { Order } from './types/filter';
+export { PaginationInput } from './types/input';
 export * from './types';
 export { PaginatedResult } from './types/pagination';
 export { nextCursor } from './functions/nextCursor';
