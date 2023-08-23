87b6527d62a9d42dddd7f53fed9c82745914d318
rlajous <rodrigo@lajous.com.ar>
2023-08-23 14:31:50 +0200

    Added script

--- publish-beta-packages.sh
+++ publish-beta-packages.sh
@@ -6,7 +6,7 @@ for pkg in "${DIRS[@]}"; do
   pkg_name=$(jq -r '.name' $pkg/package.json)
   current_version=$(jq -r '.version' $pkg/package.json)
   remote_version=$(npm view $pkg_name version 2>/dev/null)
-
+  echo $1
   
 
   if ! npm version $1; then
@@ -21,7 +21,7 @@ for pkg in "${DIRS[@]}"; do
   fi
 
   # Run the publish command
-  if ! npm publish --access public --tag beta; then
+  if ! npm publish --tag beta; then
     echo "Error: Failed to run 'npm publish --access public' in directory: packages/$pkg"
     exit 1
   fi
