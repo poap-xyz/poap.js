4c25410121a3f0b265e6b11da79fdd1a23a60d09
rlajous <rodrigo@lajous.com.ar>
2023-09-06 17:18:52 +0200

    Eslint fix

--- packages/poaps/src/index.ts
+++ packages/poaps/src/index.ts
@@ -1,7 +1,7 @@
 export { PoapsSortFields, FetchPoapsInput } from './types/input';
 export { PoapsClient } from './PoapsClient';
 export { POAP } from './domain/Poap';
-export { POAPReservation } from './domain/PoapReservation';
+export { POAPReservation } from './domain/POAPReservation';
 export { FinishedWithError } from './errors/FinishedWithError';
 export { CodeAlreadyClaimedError } from './errors/CodeAlreadyClaimedError';
 export { CodeExpiredError } from './errors/CodeExpiredError';
