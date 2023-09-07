41d453f62712ad2553ba459d710788c6b222ea8d
rlajous <rodrigo@lajous.com.ar>
2023-09-07 18:44:25 +0200

    Eslint fix

--- examples/poaps/backend/src/methods/claim_async_poap.ts
+++ examples/poaps/backend/src/methods/claim_async_poap.ts
@@ -1,54 +1,44 @@
 import { PoapsClient } from '@poap-xyz/poaps';
-import { MintingStatus, sleep } from '@poap-xyz/utils';
 import { handleError } from '../utils/handleError';
 
 /**
- * Asynchronously claims a POAP (Proof of Attendance Protocol) token using a QR hash and an address.
- * If the claim is successful, it fetches and logs the details of the claimed POAP.
- * Any errors encountered during the process are handled using a provided utility function.
+ * Attempts to claim a POAP (Proof of Attendance Protocol) token asynchronously based on a predefined QR hash and address.
+ * After successfully claiming, the function fetches and logs the details of the claimed POAP.
+ * In the event of an error during the process, the error is captured and managed by a separate utility function.
  *
- * @param {PoapsClient} client - The initialized PoapsClient to make requests.
- * @returns {Promise<void>}
+ * Note: Replace 'your_qr_hash' and 'your_address' placeholders with appropriate values.
  *
- * Usage:
- * ```typescript
- * const client = new PoapsClient(...);
- * claim_async_poap(client);
- * ```
+ * @async
+ * @function
+ * @param {PoapsClient} client - An instance of the PoapsClient to interface with the POAP service.
+ * @returns {Promise<void>} Resolves when the operation completes, either with a claimed POAP or an error.
  */
 export const claim_async_poap = async (client: PoapsClient): Promise<void> => {
   try {
     // Initiate the asynchronous claim process
     const queue_uid: string = await client.claimAsync({
       qr_hash: 'your_qr_hash',
-      address: 'yuor_address',
+      address: 'your_address',
     });
 
-    // Wait for the claim's status to move out of the 'IN_PROCESS' or 'PENDING' states
-    const status = await client.waitClaimStatus(queue_uid);
-    if (status === MintingStatus.FINISH) {
-      // Retrieve the claim code information for the QR hash
-      let checkCodeResponse = await client.getClaimCode('your_qr_hash');
+    // Wait for the claim's status to transition from 'IN_PROCESS' or 'PENDING' states
+    await client.waitClaimStatus(queue_uid);
 
-      // Poll until a result is found for the claim code
-      while (checkCodeResponse.result == null) {
-        await sleep(1000);
-        checkCodeResponse = await client.getClaimCode('your_qr_hash');
-      }
+    // Wait for the claimed POAP to be indexed and fetch the claim code information related to the QR hash
+    const getClaimCodeResponse = await client.waitPoapIndexed('your_qr_hash');
 
-      // Fetch the details of the claimed POAP and log it
-      console.log(
-        (
-          await client.fetch({
-            limit: 1,
-            offset: 0,
-            ids: [checkCodeResponse.result.token],
-          })
-        ).items[0],
-      );
-    }
+    // Retrieve and log the specifics of the claimed POAP
+    console.log(
+      (
+        await client.fetch({
+          limit: 1,
+          offset: 0,
+          ids: [getClaimCodeResponse.result.token],
+        })
+      ).items[0],
+    );
   } catch (error) {
-    // Handle any errors using the provided utility function
+    // Address any errors using the designated utility function
     handleError(error);
   }
 };
