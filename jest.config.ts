a4dae180e3e1c93d2ecd2688e70a3b5c64901889
Juan M <jm@guide42.com>
2024-01-29 07:00:11 -0300

    Enable fake timers in jest

--- jest.config.ts
+++ jest.config.ts
@@ -17,4 +17,7 @@ export default {
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   clearMocks: true,
+  fakeTimers: {
+    enableGlobally: true,
+  },
 };
