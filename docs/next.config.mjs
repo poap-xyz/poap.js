ae40467468fc8dbe6497ace98b5c857753c9777b
Juan M <jm@guide42.com>
2024-01-30 06:06:53 -0300

    Use @svgr/webpack to include SVGs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,4 +5,12 @@ const withNextra = nextra({
   themeConfig: './theme.config.tsx',
 });
 
-export default withNextra({});
+export default withNextra({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    });
+    return config;
+  },
+});
