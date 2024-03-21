7645608a84b1e0f1818f666f1808a39a72825767
Juan M <jm@guide42.com>
2024-03-21 02:28:59 -0300

    Remove repeated exports

--- packages/providers/src/core/index.ts
+++ packages/providers/src/core/index.ts
@@ -3,4 +3,5 @@ export { PoapDropApi } from './PoapDropApi/PoapDropApi';
 export { PoapCompass } from './PoapCompass/PoapCompass';
 export { AuthenticationProviderHttp } from './AuthenticationProviderHttp/AuthenticationProviderHttp';
 export { PoapTokenApi, PoapTokenApiOptions } from './PoapTokenApi/PoapTokenApi';
+
 export { InvalidMediaError } from './PoapMomentsApi/errors/InvalidMediaError';
