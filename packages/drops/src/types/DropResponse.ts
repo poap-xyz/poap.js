d1bdbdf6b9e9745e08636c76e3d1331e0d3dba76
Juan M <jm@guide42.com>
2024-04-02 00:51:40 -0300

    Search drops

---
+++ packages/drops/src/types/DropResponse.ts
@@ -0,0 +1,35 @@
+import { DropImageResponse } from './DropImageResponse';
+
+export interface DropResponse {
+  id: number;
+  fancy_id: string;
+  name: string;
+  description: string;
+  city: string;
+  country: string;
+  channel: string;
+  platform: string;
+  location_type: string;
+  drop_url: string;
+  image_url: string;
+  animation_url: string;
+  year: number;
+  start_date: string;
+  timezone: string;
+  private: boolean;
+  created_date: string;
+  expiry_date: string;
+  end_date: string;
+  stats_by_chain_aggregate: {
+    aggregate: {
+      sum: {
+        transfer_count: number;
+        poap_count: number;
+      };
+    };
+  };
+  email_claims_stats: {
+    total: number;
+  };
+  drop_image?: DropImageResponse;
+}
