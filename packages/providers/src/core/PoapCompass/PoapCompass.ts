410e198407522b5ed20268a850c188b5879e125d
Juan M <jm@guide42.com>
2024-04-12 01:12:23 -0300

    Add bad request and unauthorized errors

--- packages/providers/src/core/PoapCompass/PoapCompass.ts
+++ packages/providers/src/core/PoapCompass/PoapCompass.ts
@@ -3,6 +3,8 @@ import { CompassErrors } from '../../ports/CompassProvider/types/CompassErrors';
 import { CompassError } from '../../ports/CompassProvider/types/CompassError';
 import { CompassRequestError } from '../../ports/CompassProvider/errors/CompassRequestError';
 import { CompassMissingDataError } from '../../ports/CompassProvider/errors/CompassMissingDataError';
+import { CompassBadRequestError } from '../../ports/CompassProvider/errors/CompassBadRequestError';
+import { CompassUnauthorizedError } from '../../ports/CompassProvider/errors/CompassUnauthorizedError';
 
 const DEFAULT_COMPASS_BASE_URL = 'https://public.compass.poap.tech/v1/graphql';
 
@@ -62,17 +64,41 @@ export class PoapCompass implements CompassProvider {
       throw new Error(`Network error, received error ${error}`);
     }
 
-    if (response.status !== 200) {
-      throw new Error(
-        `Response error, received status code ${response.status}`,
-      );
-    }
+    this.handleResponseStatus(response);
 
     const body = await response.json();
 
     this.handleResponseErrors(body);
 
     return body;
+  }
+
+  /**
+   * Handles HTTP status codes and throws corresponding errors.
+   *
+   * @private
+   * @function
+   * @name PoapCompass#handleHttpStatus
+   * @param {Response} response - The response from the fetch call.
+   * @throws {CompassUnauthorizedError} for 401 Unauthorized status codes.
+   * @throws {CompassBadRequestError} for 400 Bad Request status codes.
+   * @throws {Error} for other non-200 status codes.
+   */
+  private handleResponseStatus(response: Response): void {
+    switch (response.status) {
+      case 400:
+        throw new CompassBadRequestError();
+      case 401:
+        throw new CompassUnauthorizedError();
+      case 200:
+        return; // OK
+      default:
+        // For simplicity, throwing a generic error for all other statuses.
+        // You can add more cases for other statuses as needed.
+        throw new Error(
+          `Response error, received status code ${response.status}`,
+        );
+    }
   }
 
   /**
