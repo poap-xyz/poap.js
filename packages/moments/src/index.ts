e1ad3b582e9002116b7b3a089686944ba736fcdf
Nacho <nachonegrocaino@gmail.com>
2024-05-02 11:01:29 -0300

    Add getMomentsDefaultClient

--- packages/moments/src/index.ts
+++ packages/moments/src/index.ts
@@ -3,3 +3,4 @@ export { MomentsClient } from './client/MomentsClient';
 export { CreateMomentInput } from './client/dtos/create/CreateInput';
 export { CreateSteps } from './client/dtos/create/CreateSteps';
 export { FetchMomentsInput } from './client/dtos/fetch/FetchMomentsInput';
+export { getMomentsDefaultClient } from './client/getMomentsDefaultClient';
