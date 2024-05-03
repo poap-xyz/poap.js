023c57771ac94ffc550ec8514fd89075c9f8ae71
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2024-05-03 11:05:52 -0300

    [Moments] Add MomentsClientFactory (#116)
    * Add getMomentsDefaultClient
    
    * PR Comment changes
    
    * bump versions

---
+++ packages/moments/src/client/MomentsClientFactory/options/GetMomentsDefaultClientOptions.ts
@@ -0,0 +1,58 @@
+import { AuthenticationProvider } from '@poap-xyz/providers';
+
+interface MomentsBaseOptions {
+  /**
+   * The base URL for the Moments API.
+   * @default https://moments.poap.tech
+   */
+  momentsBaseUrl?: string;
+}
+
+/**
+ * Options to create a Moments client with an authentication provider.
+ */
+export interface MomentsWithAuthenticationProvider extends MomentsBaseOptions {
+  authenticationProvider: AuthenticationProvider;
+}
+
+/**
+ * Options to create a Moments client with OAuth credentials.
+ */
+export interface MomentsWithOAuthCredentials extends MomentsBaseOptions {
+  oAuthCredentials: {
+    /**
+     * The client ID for the OAuth server.
+     */
+    clientId: string;
+    /**
+     * The client secret for the OAuth server.
+     */
+    clientSecret: string;
+    /**
+     * The domain of the OAuth server.
+     * @default https://accounts.poap.tech
+     */
+    oAuthServerDomain?: string;
+  };
+}
+
+export interface GetMomentsDefaultClientOptions {
+  /**
+   * The options to create a Moments client.
+   */
+  moments: MomentsWithAuthenticationProvider | MomentsWithOAuthCredentials;
+  /**
+   * The options to create a Compass provider.
+   */
+  compass: {
+    /**
+     * The API key for the Compass provider.
+     */
+    compassApiKey: string;
+    /**
+     * The base URL for the Compass provider.
+     * @default https://public.compass.poap.tech/v1/graphql
+     */
+    compassBaseUrl?: string;
+  };
+}
