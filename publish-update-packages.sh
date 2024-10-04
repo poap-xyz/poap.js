ebe90dcef4ecca63fd2fc4ab02e5c88416fb37a6
Rodrigo Manuel Navarro Lajous <40175251+rlajous@users.noreply.github.com>
2024-10-04 18:00:19 -0300

    fix: changed workflow copy and publish script (#136)
    * fix: changed workflow copy and publish script
    
    * fix: changed workflow copy and publish script

--- publish-update-packages.sh
+++ publish-update-packages.sh
@@ -7,6 +7,18 @@ for pkg in "${DIRS[@]}"; do
   current_version=$(jq -r '.version' $pkg/package.json)
   remote_version=$(npm view $pkg_name version 2>/dev/null)
 
+  # Change to the directory
+  if ! cd "$pkg"; then
+    echo "Error: Unable to change to directory: packages/$pkg"
+    exit 1
+  fi
+
+  # Run the build command
+  if ! npm run build; then
+    echo "Error: Failed to run 'npm run build' in directory: packages/$pkg"
+    exit 1
+  fi
+
   # Check if the package exists remotely or if the version has changed
   if [ -z "$remote_version" ] || [ "$current_version" != "$remote_version" ]; then
     if [ -z "$remote_version" ]; then
@@ -15,30 +27,19 @@ for pkg in "${DIRS[@]}"; do
       echo "Detected version change in $pkg: $remote_version -> $current_version"
     fi
 
-    # Change to the directory
-    if ! cd "$pkg"; then
-      echo "Error: Unable to change to directory: packages/$pkg"
-      exit 1
-    fi
-
-    # Run the build command
-    if ! npm run build; then
-      echo "Error: Failed to run 'npm run build' in directory: packages/$pkg"
-      exit 1
-    fi
-
     # Run the publish command
     if ! npm publish --access public; then
       echo "Error: Failed to run 'npm publish --access public' in directory: packages/$pkg"
       exit 1
     fi
 
-    # Change back to the original directory
-    if ! cd - > /dev/null; then
-      echo "Error: Unable to change back to original directory"
-      exit 1
-    fi
   else
     echo "No version change detected in $pkg"
+  fi
+
+  # Change back to the original directory
+  if ! cd - > /dev/null; then
+    echo "Error: Unable to change back to original directory"
+    exit 1
   fi
 done
