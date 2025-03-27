1c41ff76ae93ccd9bff4ffdee35bf2fd9f6acde2
Robin Gagnon <me@reobin.dev>
2025-03-27 09:27:13 -0500

    simplify profile class

--- packages/poap-sdk/src/profiles/domain/Profile.ts
+++ packages/poap-sdk/src/profiles/domain/Profile.ts
@@ -1,31 +1,22 @@
 import { ProfileResponse } from '../../providers/ports/ProfilesApiProvider';
 
 export class Profile {
-  address: string;
-  ens: string;
-  avatar: string | null;
-  header: string | null;
-
-  constructor({ address, ens, avatar, header }: Profile) {
-    this.address = address;
-    this.ens = ens;
-    this.avatar = avatar;
-    this.header = header;
-  }
+  constructor(
+    public address: string,
+    public ens: string,
+    public avatar: string | null,
+    public header: string | null,
+  ) {}
 
   public static fromResponse(
     response: ProfileResponse,
     apiUrl: string,
   ): Profile {
-    return new Profile({
-      address: response.address,
-      ens: response.ens,
-      avatar: response.records.avatar
-        ? `${apiUrl}/avatar/${response.ens}`
-        : null,
-      header: response.records.header
-        ? `${apiUrl}/header/${response.ens}`
-        : null,
-    });
+    return new Profile(
+      response.address,
+      response.ens,
+      response.records.avatar ? `${apiUrl}/avatar/${response.ens}` : null,
+      response.records.header ? `${apiUrl}/header/${response.ens}` : null,
+    );
   }
 }
