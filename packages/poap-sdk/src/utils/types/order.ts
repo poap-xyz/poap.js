667d16ead4cd32344f233a236bdacaec52edc76d
Robin Gagnon <me@reobin.dev>
2025-05-08 14:21:19 -0500

    feat: Add nulls_first and nulls_last options to Order enum (#187)

--- packages/poap-sdk/src/utils/types/order.ts
+++ packages/poap-sdk/src/utils/types/order.ts
@@ -4,6 +4,10 @@
 export enum Order {
   ASC = 'asc',
   DESC = 'desc',
+  ASC_NULLS_FIRST = 'asc_nulls_first',
+  ASC_NULLS_LAST = 'asc_nulls_last',
+  DESC_NULLS_FIRST = 'desc_nulls_first',
+  DESC_NULLS_LAST = 'desc_nulls_last',
 }
 
 export type FieldOrderBy = { [key: string]: FieldOrderBy | Order };
