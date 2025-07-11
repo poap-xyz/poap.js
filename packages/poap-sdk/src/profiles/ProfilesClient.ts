e5cfe6d88433dc467df4a509c5101285cf2aa2ed
Reversive <matiaspavan@gmail.com>
2025-07-11 13:38:55 -0300

    chore(docs): declare intent of errors map

--- packages/poap-sdk/src/profiles/ProfilesClient.ts
+++ packages/poap-sdk/src/profiles/ProfilesClient.ts
@@ -37,6 +37,10 @@ export class ProfilesClient {
    * console.log(errors.get(address2)?.message); // 'Failed to resolve data from the blockchain'
    * ```
    *
+   * Note: If a query fails due to a technical issue, it will be in the `errors` map.
+   * You can use this to implement a retry strategy for failed queries.
+   * Profiles which do not exist will not be included in the `profiles` map nor result in an error.
+   *
    * @param queries The ETH addresses or ENS.
    * @param options Options to pass to the fetch call.
    * @returns A map of input addresses (ETH address or ENS) to profiles, and a map of failed queries to errors.
